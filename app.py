599add59532599181afc25816861dc535721267f
KimTaekGwan <sbe07032@naver.com>
2023-04-11 23:08:52 +0900

    04111108

--- app.py
+++
@@ -1,6 +0,0 @@
-from project import create_app
-
-app = create_app(debug=True)
-
-if __name__ == '__main__':
-    app.run(app)
--- project/conn/db.py
+++
@@ -1,8 +0,0 @@
-# myproject/pybo/db.py
-from pymysql import cursors, connect
-
-db = connect(host='localhost',
-                user='root',
-                password='1234',
-                database='checker',
-                cursorclass=cursors.DictCursor)
